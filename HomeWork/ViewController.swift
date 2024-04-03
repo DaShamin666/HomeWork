c02eb5d0e4034feeb419036e9d77455bba153e54
Shamin Daniil <shamindaniil1129@icloud.com>
2024-04-03 19:42:33 +0300

    initial commit

---
+++ HomeWork/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  HomeWork
+//
+//  Created by Шамин Даниил Андреевич on 03.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    func homeWork() {
+        
+    }
+
+}
+
